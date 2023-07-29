41ff800458429e2eda90bdae222d2153c48adcdc
Danny Hillary <dannyhillary12@gmail.com>
2023-07-29 15:43:51 +0100

    Completed SQL 50 Question 10

---
+++ SQL50Question10.sql
@@ -0,0 +1,9 @@
+-- Question 9 of LeetCode's SQL 50 challenge.
+
+SELECT A.machine_id, round(avg(B.timestamp - A.timestamp), 3) as processing_time 
+FROM Activity A, Activity B
+WHERE  A.machine_id = B.machine_id AND A.activity_type = 'start' AND B.activity_type = 'end' AND 
+A.process_id = B.process_id 
+Group By A.machine_id;
+
+-- accepted, Runtime = 474ms
